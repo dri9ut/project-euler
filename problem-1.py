af307e260907d4b0449fabedc7accd1b70d238f3
dri9ut <dri9ut@gmail.com>
2019-11-04 11:14:39 +0300

    Edit README

--- problem-1.py
+++ problem-1.py
@@ -2,3 +2,9 @@
 # -*- coding: utf-8 -*-
 # Multiples of 3 and 5
 
+
+def multiples(interval):
+    return sum([x for x in range(1, interval) if x % 3 == 0 or x % 5 == 0])
+
+
+print(multiples(1000))
