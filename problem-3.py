183562e02bfab00b026b120a582dcc4f185aedf3
dri9ut <dri9ut@gmail.com>
2019-11-06 12:47:09 +0300

    Problems 3

--- problem-3.py
+++ problem-3.py
@@ -3,4 +3,16 @@
 
 # Largest prime factor
 
-pass
+
+def factor(num):
+    arr = [x for x in range(2, num)]
+    i = 0
+    while arr[i] < num:
+        for x in arr:
+            if x % i == 0:
+                arr.remove(x)
+        i += 1
+    return arr
+
+
+print(factor(13195))
